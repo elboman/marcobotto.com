3d773fce1bc368af3bc8add6d35fdf910c45806b
Marco Botto <marco.botto@gmail.com>
2018-04-16 13:03:36 +0100

    feat(): set language as JSX in prisms
    Taken form Reactjs.org: https://github.com/reactjs/reactjs.org/tree/master/plugins/gatsby-remark-use-jsx

---
+++ plugins/gatsby-remark-use-jsx/index.js
@@ -0,0 +1,20 @@
+const visit = require('unist-util-visit');
+
+module.exports = ({ markdownAST }) => {
+  visit(markdownAST, `code`, node => {
+    if (typeof node.lang !== 'string') {
+      return;
+    }
+    if (node.lang.indexOf('jsx') === 0) {
+      // Already JSX (with optional line range).
+      return;
+    }
+    // Turn JS into JSX, preserving the optional line range.
+    if (node.lang.indexOf('js') === 0) {
+      node.lang = 'jsx' + node.lang.substring('js'.length);
+    }
+    if (node.lang.indexOf('javascript') === 0) {
+      node.lang = 'jsx' + node.lang.substring('javascript'.length);
+    }
+  });
+};
